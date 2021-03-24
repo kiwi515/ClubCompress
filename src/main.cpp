6d0491673a598c161a0cec74d0c4ccd4e25b00fc
kiwi <schiff.trevor@gmail.com>
2021-03-23 23:29:06 -0400

    Main user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
 #include "zlib/zlib.h"
+#include "types.h"
 
-int main()
+Operation_t getOperation(u16, char**);
+void showUsage();
+
+int main(u16 argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	Operation_t task = getOperation(argc, argv);
+	
+	switch (task)
+	{
+	case INVALID:
+		showUsage();
+		return 1;
+	case COMPRESS:
+		// do compress
+		std::cout << "Compress " << argv[1] << std::endl;
+		break;
+	case DECOMPRESS:
+		// do decompress
+		std::cout << "Decompress " << argv[1] << std::endl;
+		break;
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+Operation_t getOperation(u16 argc, char** argv)
+{
+	// Argument count
+	if (argc != 3)
+	{
+		return INVALID;
+	}
+
+	// Argument strings
+	if (!argv[1] || !argv[2])
+	{
+		return INVALID;
+	}
+
+	Operation_t task = INVALID;
+	// Compress
+	task = (_stricmp(argv[1], "-C") == 0) ? COMPRESS : task;
+	// Decompress
+	task = (_stricmp(argv[1], "-D") == 0) ? DECOMPRESS : task;
+
+	// Always invalid if no filename specified
+	return (argv[2] != NULL) ? task : INVALID;
+}
+
+void showUsage()
+{
+	std::cout << "Valid usage: " << std::endl
+		<< "ClubCompress.exe -D MyCompressedFile.z" << std::endl
+		<< "ClubCompress.exe -C MyUncompressedFile.z" << std::endl;
 }
---
+++ src/types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+enum Operation_t
+{
+	INVALID = -1,
+	COMPRESS = 0,
+	DECOMPRESS = 1
+};
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
